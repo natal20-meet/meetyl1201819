75917389cdd5fb40568d508946ab2ef9b8dddd86
natal20-meet <natal20@meet.mit.edu>
2018-10-21 12:30:32 -0400

    Lab 2 and 3 done

--- lab2.py
+++ lab2.py
@@ -1,3 +1,5 @@
+import tkinter as tk 
+from tkinter import simpledialog
 a = [1,2,3,4,5]
 def make_list ():
 	x = [a[0], a[4]]
@@ -9,11 +11,26 @@ b = [-2,-1,0,1,2,3,4,5,6,7,8,9]
 def values_less_5(x): 
 	for number in b:
 		if number <=5:
-			print number
+			print(number)
+num_list2 = []
+
 #values_less_5(b) 
 c = [2,4,6,8,10,12,14,16,18]
 d = [2,6,10,14,18]
-def make_newlist():
-	for number in c & d:
-		if number 
+for y in c:
+	for x in d:
+		if y == x:
+			num_list2.append(y)
+#print(num_list2)
+
+print("write a number")
+
+answer = simpledialog.askstring("Input", "write a number",parent=tk.Tk())
+answer = int(answer)
+if answer%2 == 0:
+	print (str(answer) + "is a prime number")
+
+
+
+		
 
