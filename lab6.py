24be21d9d7c6303ff410fdb0b49698290f097e65
natal20-meet <natal20@meet.mit.edu>
2018-11-25 12:04:41 -0500

    lab6

---
+++ lab6.py
@@ -0,0 +1,20 @@
+import turtle
+from turtle import Turtle
+import random
+
+turtle.colormode(255)
+class Square(Turtle):
+	def __init__(self,size):
+		Turtle.__init__(self)
+		self.shapesize(size)
+		self.shape("square")
+	def pick_color(self):	
+		R = random.randint(0,255)
+		B = random.randint(0,255)
+		G = random.randint(0,255)
+		self.color(R,B,G)
+square1 = Square(25)
+square1.pick_color()
+		
+
+turtle.mainloop()
