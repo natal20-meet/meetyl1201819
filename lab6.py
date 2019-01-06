81b6578ce71522eb1aadebf9b4ec90b48f30cd60
natal20-meet <natal20@meet.mit.edu>
2019-01-06 12:51:15 -0500

    Indv_proj.py)

--- lab6.py
+++ lab6.py
@@ -13,18 +13,8 @@ class Square(Turtle):
 		B = random.randint(0,255)
 		G = random.randint(0,255)
 		self.color(R,B,G)
-#square1 = Square(25)
-#square1.pick_color()
-"""
-class Rectangle(object):
-	def __init__(self,width,height)
-		self.width = width
-		self.height = height
-	def make_shape(self):
-"""
-
-		
-
+square1 = Square(25)
+square1.pick_color()
 		
 
 turtle.mainloop()
